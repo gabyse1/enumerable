27276eda5661073ff7c00d74f225f8531cfeadb8
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-07 17:43:07 -0500

    Update each method in MyList class

--- MyList.rb
+++ MyList.rb
@@ -8,13 +8,13 @@ class MyList
   include MyEnumerable
 
   def each
-    i = 0;
-    until i == @list.length do
-       yield @list[i]
-      i += 1;
-    end
+    @list.each { |e| yield(e) }
   end
 end
 
 list = MyList.new([1, 2, 3, 4])
-puts list.filter { |e| e.even? }
+puts list.all? {|e| e < 5}
+puts list.all? {|e| e > 5}
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+print "#{list.filter { |e| e.even? }} \n"
