682c1e63d096e49124ae60ee1dee081f5cea25d6
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-07 21:22:31 -0500

    Set require_relative to require a module, update the each method

--- my_list.rb
+++ my_list.rb
@@ -1,18 +1,22 @@
-require './my_enumerable'
+require_relative 'my_enumerable'
 
 class MyList
-  def initialize(list)
+  def initialize(*list)
     @list = list
   end
 
   include MyEnumerable
 
-  def each(&block)
-    @list.each(&block)
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
   end
 end
 
-list = MyList.new([1, 2, 3, 4])
+list = MyList.new(1, 2, 3, 4)
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
 puts(list.any? { |e| e == 2 })
